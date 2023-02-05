077f7050346fb19d78193f9f8e055a8bdfbae872
GiladFisher <97436308+GiladFisher@users.noreply.github.com>
2023-02-06 00:24:35 +0200

    Added more colors with Array

--- really_mine_mic.ino
+++ really_mine_mic.ino
@@ -1,11 +1,15 @@
 #include <FastLED.h>
 #define IN A5
-#define THRSH 6
+#define THRSH 2
 #define DATA_PIN 2
 #define CLOCK_PIN 13
-#define NUM_LEDS 20
+#define NUM_LEDS 30
+
 
 CRGB leds[NUM_LEDS];
+//CRGB color_template[3][3] = {{CRGB(255 , 0 , 150 ),CRGB(255 /2 , 0 , 150 /2 ), CRGB(255 /3 , 0 , 150 /3 )}};
+CRGB color_template[] = {0x7F00FF,0x0000FF,0x800080,0xFF0000,0xFF7F50,0xFFA500,0xFFFF00,0x9ACD32,0x00FF00,0x00CED1,0x00BFFF};
+int to_cycle = sizeof(color_template)/sizeof(color_template[0]);
 
 int col_flag = 0;
 CRGB cur_color;
@@ -15,22 +19,12 @@ void setup() {
   Serial.begin(9600);
 }              
 void set_color(int i){
-  if(col_flag == 0){
-    cur_color = CRGB::Red;
-  }
-  else if(col_flag == 1){
-    cur_color = CRGB::Blue;
-  }
-  else{
-    cur_color = CRGB::Green;
-  }
   for(int j = 0; j < i + 1; j++){
     leds[j] = cur_color;
     FastLED.show();
   }
 }
-
-void make_dance(){
+void make_dance_single(){
   if(col_flag == 0){
     cur_color = CRGB::Red;
   }
@@ -43,9 +37,20 @@ void make_dance(){
   for(int i = 0; i < NUM_LEDS; i++){
     leds[i] = cur_color;
     FastLED.show();
-    if (listen() > THRSH && i > 4){
+    if(i > 0);
+  }
+
+  
+}
+
+void make_dance(){
+  cur_color = color_template[col_flag];
+  for(int i = 0; i < NUM_LEDS; i++){
+    leds[i] = cur_color;
+    FastLED.show();
+    if (listen() > THRSH && i > 15){
       //turn_off();
-      col_flag = (col_flag + 1) % 3;
+      col_flag = (col_flag + 1) % to_cycle;
       set_color(i);
       make_dance();
       return;
@@ -54,10 +59,11 @@ void make_dance(){
   for(int i = 0; i < NUM_LEDS; i++){
     leds[i] = CRGB::Black;
     FastLED.show(); 
-    delay(1);
+    delay(6);
   }
-  col_flag = (col_flag + 1) % 3;
+  col_flag = (col_flag + 1) % to_cycle;
 }
+
 
 int listen(){
   int vals_mean = 0;
@@ -77,8 +83,19 @@ void turn_off(){
   }
 }
 
+void shiftColorOfAll(){
+  col_flag = (col_flag + 1) % to_cycle;
+  cur_color = color_template[col_flag];
+  for(int i = 0; i < NUM_LEDS; i++){
+    leds[i] = cur_color;
+  }
+  FastLED.show();
+}
+
 void loop() {
   if(listen() > THRSH){
     make_dance();
+    //shiftColorOfAll(); delay(5);
   }
+
 }
